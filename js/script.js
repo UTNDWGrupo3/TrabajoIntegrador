f3e4ff6c0670bfa530f9c289d321ad479436a8ff
Mauricio Manresa <mauriciomanresa@hotmail.com>
2023-04-15 20:30:08 -0300

    Arreglos de Identación

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // declaro variables 
 
-
 // manejo datos de formulario de contacto 
 const frmContacto = document.getElementById("frmContacto");
 const frmPaso1 = document.getElementById("step-1");
 const frmPaso2 = document.getElementById("step-2");
- 
- 
+
 const nextBtnP1 = document.querySelector("#btnPlan-1");
 const nextBtnP2 = document.querySelector("#btnPlan-2");
 const nextBtnP3 = document.querySelector("#btnPlan-3");
@@ -17,12 +15,6 @@ const backBtn = document.querySelector("#back-btn");
 const step1 = document.querySelector("#step-1");
 const step2 = document.querySelector("#step-2");
 const summary = document.querySelector("#summary");
-
-
-
-
-
-
 
 // evento que escucha si se presiona boton asociarse plan 1
 nextBtnP1.addEventListener("click", function() {
@@ -38,61 +30,42 @@ nextBtnP1.addEventListener("click", function() {
     /* vuelvo al color original el fondo del campo */
     document.querySelector("#name-coti").style.backgroundColor = "#f9d4c6";
     document.querySelector("#email-coti").style.backgroundColor = "#f9d4c6";
-   
-
 });
-
 
 // evento que escucha si se presiona boton asociarse plan 2
 nextBtnP2.addEventListener("click", function() {
     let planSelecc =  document.querySelector("#lbl-plan-coti");
-    
     let planSeleccionado = "";
     planSeleccionado =  document.getElementById("Plan2Descripcion").textContent;
     planSelecc.innerHTML = document.getElementById("p-plan-2-titulo").textContent;
-     
     step1.style.display = "block";  
     step2.style.display = "none";
     frmPaso1.reset(); 
     /* vuelvo al color original el fondo del campo */
     document.querySelector("#name-coti").style.backgroundColor = "#f9d4c6";
     document.querySelector("#email-coti").style.backgroundColor = "#f9d4c6";
-      
-    
-   
-
 });
 
 // evento que escucha si se presiona boton asociarse plan 2
 nextBtnP3.addEventListener("click", function() {
     let planSelecc =  document.querySelector("#lbl-plan-coti");          
-    
     let planSeleccionado = "";
     planSeleccionado =  document.getElementById("Plan3Descripcion").textContent;
     planSelecc.innerHTML = document.getElementById("p-plan-3-titulo").textContent;
-     
     step1.style.display = "block";  
     step2.style.display = "none";
     frmPaso1.reset(); 
     /* vuelvo al color original el fondo del campo */
     document.querySelector("#name-coti").style.backgroundColor = "#f9d4c6";
     document.querySelector("#email-coti").style.backgroundColor = "#f9d4c6";
-   
-    
-   
-
 });
-
-
 
 // evento del boton cotizar 
 nextBtnCotizar.addEventListener("click", function() {
     let resultado;
-    
     let nombre =  document.querySelector("#name-coti").value;
     let email =  document.querySelector("#email-coti").value;
     let planSeleccionado = document.getElementById("lbl-plan-coti").textContent;
-     
     resultado = validoDatosFrmCotizacionPaso1(nombre, email, planSeleccionado);
     if (resultado === "OK")
     {
@@ -104,77 +77,56 @@ nextBtnCotizar.addEventListener("click", function() {
     {
         if (nombre=="")
         {
-             document.querySelector("#name-coti").style.backgroundColor = "#f89696";
+            document.querySelector("#name-coti").style.backgroundColor = "#f89696";
         }
         if (email=="")
         {
-             document.querySelector("#email-coti").style.backgroundColor = "#f89696";
+            document.querySelector("#email-coti").style.backgroundColor = "#f89696";
         }
-
     };
-
-
 });
 
 function muestroCotizacion(nombre, email, planSeleccionado)
 {
-    
-   let cotizacionDetalle;
-   let valorPlan;
-   let descuentoPlan;
-   let precioFinalPlan;
-
-   if (planSeleccionado=="Plan Black Individual")
-   {  
+    let cotizacionDetalle;
+    let valorPlan;
+    let descuentoPlan;
+    let precioFinalPlan;
+    if (planSeleccionado=="Plan Black Individual")
+    {  
         valorPlan = 90000;
         descuentoPlan = 30;
         precioFinalPlan =  valorPlan - (valorPlan * (descuentoPlan/100));
-         
+    }     
+};
 
-   };
-
-   if (planSeleccionado=="Plan Grupal")
-   {  
+    if (planSeleccionado=="Plan Grupal")
+    {  
         valorPlan = 150000;
         descuentoPlan = 10;
         precioFinalPlan =  valorPlan - (valorPlan * (descuentoPlan/100));
-         
+    };
 
-   };
-
-   if (planSeleccionado=="Plan Corporativo")
-   {  
+    if (planSeleccionado=="Plan Corporativo")
+    {  
         valorPlan = 110000;
         descuentoPlan = 5;
         precioFinalPlan =  valorPlan - (valorPlan * (descuentoPlan/100));
-         
+    };
 
-   };
+    cotizacionDetalle = "Plan en promoción vigiente, precio anual del plan $" + valorPlan + " , descuento especial del " + descuentoPlan + "%. Precio final con descuento incluido: $" + precioFinalPlan; 
 
-   cotizacionDetalle = "Plan en promoción vigiente, precio anual del plan $" + valorPlan + " , descuento especial del " + descuentoPlan + "%. Precio final con descuento incluido: $" + precioFinalPlan; 
-
-    
     summary.innerHTML = `Nombre: ${nombre} 
                     <br> Email: ${email} 
                     <br> Plan seleccionado ${planSeleccionado} 
                     <br> ${cotizacionDetalle}` ;
- 
+
     // ocultar el primer paso y mostrar el segundo 
     step1.style.display = "none";
     step2.style.display = "block";
 
-};
-
-
-
 frmPaso2.addEventListener("click", function(event){
-
     crearPDFCotizacion();
-
-
-
-
-
 });
 
 frmContacto.addEventListener("submit", function(event)  {
@@ -183,13 +135,8 @@ frmContacto.addEventListener("submit", function(event)  {
 
     if (validoFrmContacto() === "OK")
     {
-        //
-
-        // envio datos al pdf 
         crearPDF();
-
         // fuerzo limpieza de los campos del formulario
-
         frmContacto.reset();
 
         frmContacto.nombre.style.backgroundColor = "#f9d4c6";
@@ -198,22 +145,14 @@ frmContacto.addEventListener("submit", function(event)  {
         frmContacto.email.style.backgroundColor = "#f9d4c6";
         frmContacto.telefono.style.backgroundColor = "#f9d4c6";
         frmContacto.comentario.style.backgroundColor = "#f9d4c6";
-
-        
     };
-
-} 
-);
-
+});
 
 // valido datos ingresados del formulario cotizacion de producto
-
 function validoDatosFrmCotizacionPaso1(nombre, email)
 {
-
     let resultado="OK";
 
-    
     if (nombre == "") {
         alert("Complete el campo nombre");
         resultado = "ERR";
@@ -221,28 +160,20 @@ function validoDatosFrmCotizacionPaso1(nombre, email)
     } else if (email == "") {
         alert("Complete el campo email");
         resultado = "ERR";
-
-
     };
-    
+
     return resultado;
-
-
 };
-
 
 // valido que los campos obligatorios se encuentre cargados
 function validoFrmContacto() {
-
     let resultado="OK";
 
     if (frmContacto.nombre.value == "") {
         frmContacto.nombre.style.backgroundColor = "#f89696";
         alert("Complete el campo nombre");
-        
-        
         resultado = "ERR";
-        
+
     } else  if (frmContacto.apellido.value == "") {
         frmContacto.apellido.style.backgroundColor = "#f89696";
         alert("Complete el campo apellido");
@@ -252,36 +183,33 @@ function validoFrmContacto() {
         frmContacto.edad.style.backgroundColor = "#f89696";
         alert("Complete el campo edad");
         resultado = "ERR";
+
     } else  if (frmContacto.email.value == "") {
         frmContacto.email.style.backgroundColor = "#f89696";
         alert("Complete el campo email");
         resultado = "ERR";
+
     } else  if (frmContacto.telefono.value == "") {
         frmContacto.telefono.style.backgroundColor = "#f89696";
         alert("Complete el campo telefono");
         resultado = "ERR";    
-    } else if (frmContacto.comentario.value == "") {
 
+    } else if (frmContacto.comentario.value == "") {
         frmContacto.comentario.style.backgroundColor = "#f89696";
         alert("Complete el campo comentario");
         resultado = "ERR";
     };
-    
-    // controlo rango de edad
-    if (frmContacto.edad.value != "")
+
+// controlo rango de edad
+if (frmContacto.edad.value != "")
+{
+    if (parseInt(frmContacto.edad.value) < 1 || parseInt(frmContacto.edad.value) > 110)
     {
-        if (parseInt(frmContacto.edad.value) < 1 || parseInt(frmContacto.edad.value) > 110)
-        {
-            frmContacto.edad.style.backgroundColor = "#f89696";
-            alert("Debe ingresar una edad  valida. (rango 1 a 110). Gracias")
-            resultado = "ERR";
-        }
-        
-    };
-
+        frmContacto.edad.style.backgroundColor = "#f89696";
+        alert("Debe ingresar una edad  valida. (rango 1 a 110). Gracias")
+        resultado = "ERR";
+    }
     return resultado;
-
-
 };
 
 // funcion para generar PDF
@@ -298,7 +226,6 @@ function crearPDF()
     let imgLogo = new Image();
     imgLogo.src = "./image/logo.jpg";
 
-    
     // creo un objeto nuevo date 
     let today = new Date();
 
@@ -311,18 +238,12 @@ function crearPDF()
     segundos = date.getSeconds();
     milisegundos = date.getMilliseconds();
 
-
-    
-    
     var doc = new jsPDF();
-    
     let y = 10;
-
     doc.text(0,y = y + 10,"_______________________________________________________________________________");
     doc.addImage(imgLogo,"JPEG", 5, y = y + 5, 30,30); 
     doc.text(50,y = y + 20, "G R U P O   I I I   F I T N E S S");
     doc.text(0,y = y + 10,"________________________________________________________________________________");
-    
     
     doc.text(20,y = y + 10,"Nombre          : " + nombre);
     doc.text(20,y = y + 10,"Apellido          : " + apellido);
@@ -334,7 +255,6 @@ function crearPDF()
     doc.text(20,y = y + 10, "Mensaje generado el: " + fecha + ' a las ' + horas + ":" + minutos + ":" + segundos );
 
     doc.save('DatosContacto'+'_'+apellido+nombre+'_'+fecha+'_'+horas+minutos+segundos+milisegundos+'.pdf');
- 
 };
 
 function crearPDFCotizacion()
@@ -350,7 +270,6 @@ function crearPDFCotizacion()
     let imgLogo = new Image();
     imgLogo.src = "./image/logo.jpg";
 
-    
     // creo un objeto nuevo date 
     let today = new Date();
 
@@ -362,27 +281,18 @@ function crearPDFCotizacion()
     minutos = date.getMinutes();
     segundos = date.getSeconds();
     milisegundos = date.getMilliseconds();
-
-
-    
     
     var doc = new jsPDF('landscape');
-    
     let y = 10;
-
     doc.setFontSize(10);
     doc.text(0,y = y + 10,"_____________________________________________________________________________________________________________________________________________________");
     doc.addImage(imgLogo,"JPEG", 5, y = y + 5, 30,30); 
-    
     doc.text(50,y = y + 20, "G R U P O   I I I   F I T N E S S");
     doc.text(50,y = y + 20, "Solicitud de cotización");
     doc.text(0,y = y + 10,"_____________________________________________________________________________________________________________________________________________________");
     
-    
     doc.text(1,y = y + 10, summary);
     doc.text(1,y = y + 80,"_____________________________________________________________________________________________________________________________________________________");
     doc.text(20,y = y + 10, "Mensaje generado el: " + fecha + ' a las ' + horas + ":" + minutos + ":" + segundos );
-
     doc.save('Cotizacion'+fecha+'_'+horas+minutos+segundos+milisegundos+'.pdf');
- 
 };
